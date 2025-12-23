41c3af7b312099abc57022b7aa56c6be8bc6d67f
elisabelousova <elizabethbelousova@gmail.com>
2025-12-23 11:19:57 +0300

    Backend + API
    - Отображение списка приёмов пищи
    - Добавление нового приёма пищи через модальное окно
    - Получение списка приёмов пищи с сервера (GET)
    - Отправка нового приёма пищи на сервер (POST)
    - Подсчёт процента дневной нормы калорий (из расчёта 2000 ккал)

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,29 +1,48 @@
 import { useState } from "react";
 import styles from "./Modal.module.css";
 
-export default function Modal({ isOpen, onClose }) {
-  const [value, setValue] = useState("");
+export default function Modal({ isOpen, onClose, onSave }) {
+  const [name, setName] = useState("Завтрак");
+  const [kcal, setKcal] = useState("");
+  const [B, setB] = useState("");
+  const [J, setJ] = useState("");
+  const [U, setU] = useState("");
 
   if (!isOpen) return null;
 
+  function handleSave() {
+    onSave({
+  mealType, // "Завтрак" | "Обед" | "Ужин"
+  kcal,
+  B,
+  J,
+  U
+});
+
+    setKcal("");
+    setB("");
+    setJ("");
+    setU("");
+  }
+
   return (
     <div className={styles.overlay} onClick={onClose}>
-      <div
-        className={styles.modal}
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
         <h3 className={styles.title}>Прием пищи</h3>
 
-        <input
-          className={styles.input}
-          placeholder="Введите блюдо"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
+        <select className={styles.input} value={name} onChange={(e) => setName(e.target.value)}>
+          <option>Завтрак</option>
+          <option>Обед</option>
+          <option>Ужин</option>
+          <option>+ Ещё</option>
+        </select>
 
-        <button className={styles.closeBtn} onClick={onClose}>
-          Закрыть
-        </button>
+        <input className={styles.input} placeholder="Ккал" onChange={(e) => setKcal(e.target.value)} />
+        <input className={styles.input} placeholder="Б" onChange={(e) => setB(e.target.value)} />
+        <input className={styles.input} placeholder="Ж" onChange={(e) => setJ(e.target.value)} />
+        <input className={styles.input} placeholder="У" onChange={(e) => setU(e.target.value)} />
+
+        <button className={styles.saveBtn} onClick={handleSave}>Сохранить</button>
       </div>
     </div>
   );
